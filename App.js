bcb17f1034996090ce237d8afd0a953429697eef
Gustavo Lopes <gustavolopesdev14@gmail.com>
2025-04-30 14:36:40 -0300

    comit One

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList, ActivityIndicator } from "react-native";
+
+import api from './src/services/api';
+import Filmes from './src/Filmes'
+
+export default function App() {
+  const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    async function loadFilmes() {
+      try {
+        const response = await api.get('r-api/?api=filmes');
+        //console.log(response.data);
+        setFilmes(response.data);
+        setLoading(false)
+      } catch (error) {
+        console.error('Erro ao buscar os filmes:', error);
+      }
+    }
+
+    loadFilmes();
+  }, []);
+
+  if(loading){
+    return(
+      <View style={ { alignItems:'center', justifyContent:'center', flex:1   }}>
+        <ActivityIndicator  color='#121212' size={45} />
+
+      </View>
+    )
+  }else{
+    return (
+      <View style={styles.container}>
+  
+        <FlatList
+        data={filmes}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item })=>  <Filmes data={item}/>}
+        />
+      </View>
+    );
+  }
+
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  }
+});
+
